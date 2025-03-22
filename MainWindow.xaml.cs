9b809edbf7cf64072c98ef94066434f8cbe1aabf
szymon-skalmierski <xketris@gmail.com>
2025-03-22 17:10:40 +0100

    button methods and properties

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,8 +18,30 @@ namespace EmployeeManager;
 /// </summary>
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
+
+    private string? Contract;
+
+    public MainWindow() {
         InitializeComponent();
+        Contract = "Umowa na czas nieokreślony";
+    }
+
+    private void AddEmployee(object sender, RoutedEventArgs e) {
+        Debug.WriteLine($"Imie: {FirstName.Text}, Nazwisko: {LastName.Text}, DateOfBirth: {DateOfBirth.Text},  Salary: {Salary.Text}, Position: {Position.Text}, Contract: {Contract}");
+        Debug.WriteLine("Employee has been added");
+    }
+    private void SaveChangedEmployee(object sender, RoutedEventArgs e) {
+        Debug.WriteLine("Employee has been saved");
+    }
+    private void SaveEmployees(object sender, RoutedEventArgs e) {
+        Debug.WriteLine("Employees has been saved");
+    }
+    private void LoadEmployees(object sender, RoutedEventArgs e) {
+        Debug.WriteLine("Employees has been loaded");
+    }
+
+    private void SetEmployeeContract(object sender, RoutedEventArgs e) {
+        RadioButton option = (RadioButton) sender;
+        Contract = option.Content?.ToString();
     }
 }
